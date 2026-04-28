0028c08d74c4dfa8e502d2bb44a9931758007e15
Albedo-AB <di.aravenab@duocuc.cl>
2026-04-28 13:09:04 -0400

    Model,Controller,Service,Repository Creados.
    No se puede continuar hasta la creacion de boleta y el arreglo de admusuario.

---
+++ mi-inventario/ms-ventas/src/main/java/com/mi_inventario/ms_ventas/model/ventas.java
@@ -0,0 +1,14 @@
+package com.mi_inventario.ms_ventas.model;
+
+import java.time.*;
+import jakarta.*;
+
+public class ventas {
+
+    private Long id;
+    private LocalDate fecha;
+    private double montoTotal;
+    private admusuario admusuario;
+    private boleta boleta;
+
+}
