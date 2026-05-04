14dcbdd90b34cd8752cd2bab8f12adc7383f46b2
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 17:29:19 -0400

    :3

--- mi-inventario/ms-boleta/src/main/java/com/mi_inventario/ms_boleta/service/boletaService.java
+++ mi-inventario/ms-boleta/src/main/java/com/mi_inventario/ms_boleta/service/boletaService.java
@@ -1,4 +1,33 @@
 package com.mi_inventario.ms_boleta.service;
 
+import com.mi_inventario.ms_boleta.model.Boleta;
+import com.mi_inventario.ms_boleta.model.ESTADO;
+import com.mi_inventario.ms_boleta.repository.boletaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.time.LocalDate;
+
+@Service
 public class boletaService {
+
+    @Autowired
+    private boletaRepository brep;
+
+    private List<Boleta> obtenerInfo(){
+        return this.brep.infoBoleta();
+    }
+
+    private Boleta crearBoleta(Boleta bo){
+        bo.setFecha(LocalDate.now());
+        if (bo.getEstado() == null){
+            bo.setEstado(ESTADO.PENDIENTE);
+        }
+        return this.brep.registrarBoleta(bo);
+    }
+
+    private void eliminarBoleta(Long id) {
+        this.brep.eliminarBoleta(id);
+    }
+
 }
