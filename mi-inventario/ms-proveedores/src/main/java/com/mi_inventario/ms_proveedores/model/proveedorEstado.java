a53a6aa3495dc4c0982ba2816b2acfefe98eee37
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-13 09:29:14 -0400

    proveedor

---
+++ mi-inventario/ms-proveedores/src/main/java/com/mi_inventario/ms_proveedores/model/proveedorEstado.java
@@ -0,0 +1,8 @@
+package com.mi_inventario.ms_proveedores.model;
+
+public enum proveedorEstado {
+
+    DISPONIBLE,
+    NO_DISPONIBLE
+
+}
---
+++ mi-inventario/ms-proveedores/src/main/java/com/mi_inventario/ms_proveedores/service/proveedorService.java
@@ -0,0 +1,39 @@
+package com.mi_inventario.ms_proveedores.service;
+
+import com.mi_inventario.ms_proveedores.model.proveedorModel;
+import com.mi_inventario.ms_proveedores.model.proveedorEstado;
+import com.mi_inventario.ms_proveedores.repository.proveedorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class proveedorService {
+    private final proveedorRepository repo;
+
+    //Get
+    public List<proveedorModel> verProveedores(){return repo.infoProveedor();}
+
+    //Post
+    public proveedorModel registrarProveedor(proveedorModel p){
+        p.setEstado(proveedorEstado.NO_DISPONIBLE);
+        return repo.guardarProveedor(p);
+    }
+    //Delete
+    public void eliminarProveedor(Long id){repo.eliminar(id);}
+
+    //Update
+    public proveedorModel actualizarProveedor(Long id, proveedorModel p){
+        proveedorModel pModel = repo.buscarProveedorPorId(id);
+
+        if (pModel!=null){
+            throw new RuntimeException("no disponible/no existe" );
+        }
+        p.setEstado(p.getEstado());
+
+        return repo.guardarProveedor(p);
+    }
+}
